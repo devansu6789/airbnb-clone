a77ad09f40b434fdf4db685c967e9a162f272a8b
devansu6789 <newid8974@gmail.com>
2022-05-24 01:06:58 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { ReactComponent as Logo } from "../assets/logo.svg";
+
+const Navbar = () => {
+  return (
+    <div>
+      <Logo />
+    </div>
+  );
+};
+
+export default Navbar;
